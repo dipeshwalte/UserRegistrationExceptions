5289e3adcb9d907b5c3aa3caa8b28aede69281cc
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2021-02-09 15:32:04 +0530

    Add project files.

---
+++ UserRegistrationExceptions/InvalidInputException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserRegistrationExceptions
+{
+    /// <summary>
+    /// Custom exception class for mood analysis
+    /// </summary>
+    /// <seealso cref="System.Exception" />
+    class InvalidInputException : Exception
+    {
+        public InvalidInputException()
+        {
+
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MoodAnalysisException"/> class.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <param name="name">The name.</param>
+        public InvalidInputException(string name) : base(name)
+        {
+            
+        }
+    }
+}
---
+++ UserRegistrationExceptions/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationExceptions
+{
+    class Program
+    {
+        static void ValidatePatterns(string pattern, string[] inputs)
+        {
+            Console.WriteLine("------------------------------------------");
+            Regex re = new Regex(pattern);
+            foreach (string input in inputs)
+            {
+                if (re.IsMatch(input))
+                    Console.WriteLine($"{input} is Valid");
+                else
+                    Console.WriteLine($"{input} is inValid");
+            }
+            Console.WriteLine("-----------------------------------------");
+        }
+        static void Main(string[] args)
+        {
+            string firstNamePattern = "[A-Z][a-z]{2,}";
+            string[] firstNameInputs = { "dipesh", "Dipesh", "Di", "Dinesh", "Dha" };
+            ValidatePatterns(firstNamePattern, firstNameInputs);
+
+            string lastNamePattern = "[A-Z][a-z]{2,}";
+            string[] lastNameInputs = { "walte", "Walte", "Wa", "Zokhowizh", "Wha" };
+            ValidatePatterns(lastNamePattern, lastNameInputs);
+
+            string emailPattern = @"^[0-9a-zA-Z]+[\-\.+]?[A-Za-z0-9]*@[0-9A-Za-z]+\.[a-zA-Z]{2,4}\.?([a-zA-Z]{2,4})?$";
+            string[] emailInputs = { "abc.xyz@bl.co.in", "abc@bl.co", "abc@bl", "abc@bc.com" };
+            ValidatePatterns(emailPattern, emailInputs);
+
+            string phonePattern = @"^[0-9]{2}\s[0-9]{10}$";
+            string[] phoneInputs = { "91 9422421317", "54 942242131", "9422421315", "9 9422421317" };
+            ValidatePatterns(phonePattern, phoneInputs);
+
+            string passwordPattern = @"(?=.*[A-Z])(?=.*[0-9])(?=[^.,:;'!@#$%^&*_+=|(){}[?\-\]\/\\]*[.,:;'!@#$%^&*_+=|(){}[?\-\]\/\\][^.,:;'!@#$%^&*_+=|(){}[?\-\]\/\\]*$)[0-9A-Za-z!@#\$%\^&\*\(\)\-\+]{8,}";
+            string[] passwordInputs = { "1A!2345678", "1!!Asrwerds", "fdsaf", "fetsetv!", "12345678A" };
+            ValidatePatterns(passwordPattern, passwordInputs);
+
+            string emailPattern2 = @"^[0-9a-zA-Z]+[\-\.+]?[A-Za-z0-9]+@[0-9A-Za-z]+\.[a-zA-Z]{2,4}\.?([a-zA-Z]{2,4})?$";
+            string[] emailAllInputs = { "abc@yahoo.com",
+                                        "abc-100@yahoo.com",
+                                        "abc.100@yahoo.com",
+                                        "abc111@abc.com",
+                                        "abc-100@abc.net",
+                                        "abc.100@abc.com.au",
+                                        "abc@1.com",
+                                        "abc@gmail.com.com",
+                                        "abc+100@gmail.com",
+                                        "abc",
+                                        "abc@.com.my",
+                                        "abc123@gmail.a",
+                                        "abc123@.com",
+                                        "abc123@.com.com",
+                                        ".abc@abc.com",
+                                        "abc()*@gmail.com",
+                                        "abc@%*.com",
+                                        "abc..2002@gmail.com",
+                                        "abc.@gmail.com",
+                                        "abc@abc@gmail.com",
+                                        "abc@gmail.com.1a",
+                                        "abc@gmail.com.aa.au",
+                                           };
+            ValidatePatterns(emailPattern2, emailAllInputs);
+        }
+    }
+}
